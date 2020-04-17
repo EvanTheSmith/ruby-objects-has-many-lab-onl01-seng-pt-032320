433e7397599ac241e8df0e48299bc5a0635d0f84
Evan Smith <professionalemail@zoho.com>
2020-04-17 04:17:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,24 @@
 class Author
   attr_accessor :name
+ 
   def initialize(name)
     @name = name
-    @posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_name(name)
+    post = Post.new(name)
+    add_post(post)
+  end
+ 
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+  
+  def self.post_count
+    Post.all.size
   end
 end
