cb50c4fa6269b5cba2343049080635a1dbfed50b
Evan Smith <professionalemail@zoho.com>
2020-04-17 03:44:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,8 @@
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_accessor :name, :artist
   
-  def initialize(name, genre)
+  def initialize(name)
     @name = name
-    @genre = genre
   end
   
 end
