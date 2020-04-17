a5c27d07a32b937912a75d0046e95906fda68a95
Evan Smith <professionalemail@zoho.com>
2020-04-17 03:38:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,9 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@all = []
   
   def initialize(name, genre)
     @name = name
     @genre = genre
-    @all << self
   end
   
-  def self.all
-    @@all
-  end
 end
