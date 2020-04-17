53ba82b772e3e38fd241a698b7a46a1af3ee73ee
Evan Smith <professionalemail@zoho.com>
2020-04-17 03:30:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,16 +5,4 @@ class Artist
     @name = name
   end
   
-  def add_song(song)
-    song.artist = self
-  end
-  
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
-    add_song(song)
-  end
-  
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
 end
